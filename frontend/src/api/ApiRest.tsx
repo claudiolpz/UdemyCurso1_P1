dedb6c796c037229a73b16a81d4bd27f451b8eea
claudiolpz <claudio.lopez1@mail.udp.cl>
2025-05-19 21:21:10 -0400

    Login

--- frontend/src/api/ApiRest.tsx
+++ frontend/src/api/ApiRest.tsx
@@ -1,7 +1,11 @@
 import api from '../config/axios';
-import type {RegisterForm} from '../types'
+import type {RegisterForm, LoginForm} from '../types'
 
 export async function authRegister(request: RegisterForm) {
     const response = await api.post(`/auth/register`, request);
+    return response;
+}
+export async function authLogin(request: LoginForm) {
+    const response = await api.post(`/auth/login`, request);
     return response;
 }
--- frontend/src/pages/RegisterView.tsx
+++ frontend/src/pages/RegisterView.tsx
@@ -4,30 +4,30 @@ import { authRegister } from "../api/ApiRest";
 import { isAxiosError } from "axios";
 import { toast } from "sonner";
 import ErrorMessage from "../components/ErrorMessage";
-import type {RegisterForm} from '../types'
+import type { RegisterForm } from '../types'
 const RegisterView = () => {
 
-  const initialvalues : RegisterForm = {
-    name:'',
-    email:'',
-    handle:'',
-    password:'',
-    password_confirmation:''
+  const initialvalues: RegisterForm = {
+    name: '',
+    email: '',
+    handle: '',
+    password: '',
+    password_confirmation: ''
   }
-  const { register, watch, handleSubmit, formState:{errors}} = useForm({defaultValues: initialvalues});
-  
+  const { register, watch, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialvalues });
+
   const password = watch('password')
 
-   const handleRegister = async (formData: RegisterForm) => {
+  const handleRegister = async (formData: RegisterForm) => {
     try {
       const response = await authRegister(formData);
       if (response.status === 201) {
         toast.success('Usuario registrado exitosamente!')
       }
     } catch (error) {
-      if(isAxiosError(error) && error.response){
-        toast.error(error.response.data.error || 'Error al registrar usuario');
-      }      
+      if (isAxiosError(error) && error.response) {
+        toast.error(error.response.data.error);
+      }
     }
   }
   return (
@@ -46,8 +46,8 @@ const RegisterView = () => {
             id="name"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="John Doe"
-            {...register('name',{
-              required:"El nombre es obligatorio"
+            {...register('name', {
+              required: "El nombre es obligatorio"
             })}
           />
           {/* {errors.name && String(errors.name.message)} */}
@@ -65,8 +65,8 @@ const RegisterView = () => {
             id="email"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="name@flowbite.com"
-            {...register('email',{
-              required:"El E-mail es obligatorio",
+            {...register('email', {
+              required: "El E-mail es obligatorio",
               pattern: {
                 value: /\S+@\S+\.\S+/,
                 message: "E-mail no válido",
@@ -75,7 +75,7 @@ const RegisterView = () => {
           />
           {errors.email && <ErrorMessage>{String(errors.email.message)}</ErrorMessage>}
         </div>
-         <div className="mb-5">
+        <div className="mb-5">
           <label
             htmlFor="handle"
             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -87,8 +87,8 @@ const RegisterView = () => {
             id="handle"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="JohnDoe"
-            {...register('handle',{
-              required:"El handle es obligatorio"
+            {...register('handle', {
+              required: "El handle es obligatorio"
             })}
           />
           {errors.handle && <ErrorMessage>{String(errors.handle.message)}</ErrorMessage>}
@@ -104,11 +104,11 @@ const RegisterView = () => {
             type="password"
             id="password"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            {...register('password',{
-              required:"La contraseña es obligatoria",
-              minLength:{
-                value:8,
-                message:"La constraseña debe tener minimo 8 caracteres"
+            {...register('password', {
+              required: "La contraseña es obligatoria",
+              minLength: {
+                value: 8,
+                message: "La constraseña debe tener minimo 8 caracteres"
               }
             })}
           />
@@ -125,14 +125,14 @@ const RegisterView = () => {
             type="password"
             id="password_confirmation"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            {...register('password_confirmation',{
-              required:"La contraseña es obligatoria",
+            {...register('password_confirmation', {
+              required: "La contraseña es obligatoria",
               validate: (value) => value === password || 'Las contraseña no son iguales'
             })}
           />
           {errors.password_confirmation && <ErrorMessage>{String(errors.password_confirmation.message)}</ErrorMessage>}
         </div>
-       
+
         <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
